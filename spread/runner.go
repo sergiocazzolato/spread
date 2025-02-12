220233efd1f9b687643231323d679843cdabd101
katie <katie.may@canonical.com>
2025-02-12 16:26:09 +0100

    added project-level artifacts

--- spread/runner.go
+++ spread/runner.go
@@ -22,24 +22,25 @@ import (
 )
 
 type Options struct {
-	Password       string
-	Filter         Filter
-	Reuse          bool
-	ReusePid       int
-	Debug          bool
-	NoDebug        bool
-	Shell          bool
-	ShellBefore    bool
-	ShellAfter     bool
-	Abend          bool
-	Restore        bool
-	Resend         bool
-	Discard        bool
-	Artifacts      string
-	Logs           string
-	Seed           int64
-	Repeat         int
-	GarbageCollect bool
+	Password         string
+	Filter           Filter
+	Reuse            bool
+	ReusePid         int
+	Debug            bool
+	NoDebug          bool
+	Shell            bool
+	ShellBefore      bool
+	ShellAfter       bool
+	Abend            bool
+	Restore          bool
+	Resend           bool
+	Discard          bool
+	Artifacts        string
+	Logs             string
+	Seed             int64
+	Repeat           int
+	GarbageCollect   bool
+	ProjectArtifacts string
 }
 
 type Runner struct {
@@ -703,6 +704,12 @@ func (r *Runner) worker(backend *Backend, system *System, order []int) {
 		insideBackend = false
 	}
 	if !abend && insideProject {
+		if err := r.fetchProjectArtifacts(client); err != nil {
+			printf("Cannot copy contents %v", err)
+			r.tomb.Killf("cannot copy contents: %v", err)
+		}
+	}
+	if !abend && insideProject {
 		if !r.run(client, last, restoring, r.project, r.project.Restore, r.project.Debug, &abend) {
 			r.add(&stats.ProjectRestoreError, last)
 		}
@@ -716,6 +723,38 @@ func (r *Runner) worker(backend *Backend, system *System, order []int) {
 		printf("Discarding %s...", server)
 		r.discardServer(server)
 	}
+}
+
+func (r *Runner) fetchProjectArtifacts(client *Client) error {
+	if r.options.ProjectArtifacts == "" || len(r.project.Artifacts) == 0 {
+		return nil
+	}
+
+	localDir := filepath.Join(r.options.ProjectArtifacts)
+	if err := os.MkdirAll(localDir, 0755); err != nil {
+		return fmt.Errorf("cannot create artifacts directory: %v", err)
+	}
+
+	tarr, tarw := io.Pipe()
+
+	var stderr bytes.Buffer
+	cmd := exec.Command("tar", "xJ")
+	cmd.Dir = localDir
+	cmd.Stdin = tarr
+	cmd.Stderr = &stderr
+	err := cmd.Start()
+	if err != nil {
+		return fmt.Errorf("cannot start unpacking tar: %v", err)
+	}
+
+	printf("Fetching artifacts")
+
+	remoteDir := filepath.Join(r.project.RemotePath)
+	err = client.RecvTar(remoteDir, r.project.Artifacts, tarw)
+	tarw.Close()
+	terr := cmd.Wait()
+
+	return firstErr(err, terr)
 }
 
 func (r *Runner) job(backend *Backend, system *System, suite *Suite, last *Job, order []int) *Job {
